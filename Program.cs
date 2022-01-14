e4a841582f6e1e632b2a7cb71a3ba9c5bd5f2030
sanwar786 <95489644+sanwar786@users.noreply.github.com>
2022-01-14 12:07:21 +0530

    Fibonocci Series

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TestHelloProject102;
+
+Console.WriteLine("Hello, World!");
+ReverseNumber.FindReverseNumber();
---
+++ ReverseNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHelloProject102
+{
+    internal class ReverseNumber
+    {
+        //syntax- accessModifier static returtype methodName
+        public static void FindReverseNumber()
+        {
+            Console.WriteLine("Please Enter Number To Reversr");
+            int number=Convert.ToInt32(Console.ReadLine()); //123 //321
+            int reminder, reverse = 0;
+            while(number>0)
+            {
+                reminder = number % 10; //3 2 1
+                reverse = (reverse * 10) + reminder; //3 32 320+1=321
+                number = number / 10; //12 1 0
+            }
+            Console.WriteLine("After reverse:" + reverse);
+        }
+    }
+}
